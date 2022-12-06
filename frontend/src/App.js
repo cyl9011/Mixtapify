d566f346cd87191b1e1360d428a639d78abbce35
cyl9011 <yolei2363@gmail.com>
2022-12-06 13:35:32 -0500

    Revert "Merge branch 'main' into yolei/design-cassette"
    This reverts commit 8ade049c2e74cef817f13fdec8144e418ca71693, reversing
    changes made to f7a18324425d75e582afd1512116de873a5dd93f.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,24 @@
-import React, { useState, useRef, useEffect } from "react";
-
-import { Route, Routes, Router, Navigate } from "react-router-dom";
-import appRoutes from "./shared/appRoutes";
-
-import NavBar from "./containers/NavBar/NavBar";
-import Home from "./containers/Home/Home";
-import Footer from "./containers/Footer/Footer";
-import Sender from "./containers/Sender/Sender";
-import Recipient from "./containers/Recipient/Recipient";
-import Title from "./containers/Title/Title";
-import Decorate from "./containers/Decorate/Decorate";
-import Choose from "./containers/Choose/Choose";
-import Cassette from "./containers/Cassette/Cassette";
-import NotImplemented from "./containers/NotImplemented/NotImplemented";
-import DesignCassette from "./pages/DesignCassette";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Build from "./pages/Build/Build";
-
+import DesignCassette from "./pages/DesignCassette";
 import "./App.css";
 
-let globalItems = 0;
+function App() {
+  const [token, setToken] = useState("");
 
-const App = () => {
-  // SSUI Lab 7
-  const [cartItems, setCartItems] = useState([]);
-  const [newShirt, setNewShirt] = useState();
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
 
-  function updateNewShirt(e) {
-    setNewShirt(e.target.value);
-  }
+    getToken();
+  }, []);
 
-  function createNewShirt() {
-    setCartItems([...cartItems, { text: newShirt, id: ++globalItems }]);
-  }
-
-  function deleteShirt(id) {
-    let tempCartItems = [...cartItems];
-    tempCartItems.splice(
-      tempCartItems.findIndex((d) => d.id === d), 1
-    );
-    setCartItems(tempCartItems);
-  }
-
-  return (
-    <div className="App">
-      <div className="MainContent">
-        <NavBar />
-        <Routes>
-          <Route path={appRoutes.home} element={<Home />} />
-          <Route path={appRoutes.sender} element={<Sender />} />
-          <Route path={appRoutes.recipient} element={<Recipient />} />
-          <Route path={appRoutes.title} element={<Title />} />
-          <Route path={appRoutes.decorate} element={<Decorate />} />
-          <Route path={appRoutes.choose} element={<Choose />} />
-          <Route path={appRoutes.cassette} element={<Cassette />} />
-          <Route path={appRoutes.notImplemented} element={<NotImplemented />} />
-        </Routes>
-      </div>
-      <Footer />
-    </div>
-  );
-};
+  // return <>{token === "" ? <Login /> : <Build token={token} />}</>;
+  return <DesignCassette />
+}
 
 export default App;
